d1a3df3e9ac0c6762904fc1228b578d6333db482
Guilherme Gomes Barboza <guilherme.barboza@bandtec.com.br>
2023-07-19 01:47:18 -0300

    add: config server complete

---
+++ hyperxpress-config-server/src/test/java/br/com/guigb9/hyperxpressconfigserver/HyperxpressConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.guigb9.hyperxpressconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyperxpressConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
